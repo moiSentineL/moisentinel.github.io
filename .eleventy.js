739c654db3e85a475810f2cccab5d3c56fa6aa1b
Nibir Sankar <68242099+moiSentineL@users.noreply.github.com>
2024-02-13 21:27:26 +0530

    add previous and next blogs

--- .eleventy.js
+++ .eleventy.js
@@ -29,6 +29,20 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.setLibrary("md", markdownLib);
 
+    eleventyConfig.addCollection("posts", function(collection) {
+        const coll = collection.getFilteredByTag("post");
+    
+        for(let i = 0; i < coll.length ; i++) {
+            const prevPost = coll[i-1];
+            const nextPost = coll[i + 1];
+    
+            coll[i].data["prevPost"] = prevPost;
+            coll[i].data["nextPost"] = nextPost;
+        }
+    
+        return coll;
+    });
+
     return {
         dir: {
           input: 'src',
