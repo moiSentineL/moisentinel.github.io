d33c455de9cb4fb78a5ef178c2634ecfc494e2d2
Nibir Sankar <68242099+moiSentineL@users.noreply.github.com>
2024-02-27 18:09:31 +0530

    add anchor links

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,44 @@
 const { DateTime } = require("luxon");
 const markdownIt = require("markdown-it");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+const markdownItAnchor = require('markdown-it-anchor');
+
+const position = {
+    false: "push",
+    true: "unshift",
+}
+  
+  // Copied directly from the plugin source code, with one edit
+  // (marked with comments)
+const renderPermalink = (slug, opts, state, idx) => {
+    const space = () =>
+      Object.assign(new state.Token("text", "", 0), {
+        content: " ",
+      })
+  
+    const linkTokens = [
+      Object.assign(new state.Token("link_open", "a", 1), {
+        attrs: [
+          ["class", opts.permalinkClass],
+          ["href", opts.permalinkHref(slug, state)],
+        ],
+      }),
+      Object.assign(new state.Token("html_block", "", 0), {
+        // Edit starts here:
+        content: `<span aria-hidden="true" class="header-anchor__symbol">#</span>
+        <span class="screen-reader-only">Direct link to this section</span>`,
+        // Edit ends
+      }),
+      new state.Token("link_close", "a", -1),
+    ]
+  
+    if (opts.permalinkSpace) {
+      linkTokens[position[!opts.permalinkBefore]](space())
+    }
+    state.tokens[idx + 1].children[position[opts.permalinkBefore]](
+      ...linkTokens
+    )
+}
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(pluginRss);
@@ -21,7 +59,11 @@ module.exports = function(eleventyConfig) {
         breaks: true,
         linkify: true
     };
-    let markdownLib = new markdownIt(markdownOptions);
+    const markdownItAnchorOptions = {
+        permalink: true,
+        renderPermalink
+    }
+    let markdownLib = new markdownIt(markdownOptions).use(markdownItAnchor, markdownItAnchorOptions);
 
     //Add div around tables
     markdownLib.renderer.rules.table_open = () => '<div class="table-wrapper">\n<table>\n',
