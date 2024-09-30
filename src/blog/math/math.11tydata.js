b6a7dd9888459a82b57c16608da95bb752e37ac2
moiSentineL <nibir.patowary@gmail.com>
2024-09-30 14:18:19 +0530

    better mathventures code

--- src/blog/math/math.11tydata.js
+++ src/blog/math/math.11tydata.js
@@ -1,21 +1,3 @@
-
-const collection = (eleventyConfig) => {
-
-  eleventyConfig.addCollection("mathposts", function(collection) {
-    const coll = collection.getFilteredByTag("math");
-
-    for(let i = 0; i < coll.length ; i++) {
-        const prevPostmath = coll[i-1];
-        const nextPostmath = coll[i + 1];
-
-        coll[i].data["prevPostmath"] = prevPostmath;
-        coll[i].data["nextPostmath"] = nextPostmath;
-    }
-
-    return coll;
-});
-}
-
 module.exports = {
     eleventyComputed: {
       title: data => data.title || data.page.filePathStem.split('/').pop(),
